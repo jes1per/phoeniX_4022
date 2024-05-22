ad92c7253bce1b0ec3251c5b8982561652b618d7
jes1per <ma.mirzaabbasi@gmail.com>
2024-05-22 12:06:52 +0330

    Add changes to bubble_sort to see the output

--- phoeniX_Testbench.v
+++ phoeniX_Testbench.v
@@ -2,7 +2,7 @@
 `include "phoeniX.v"
 
 `ifndef FIRMWARE
-    `define FIRMWARE "Software\\User_Codes\\test\\test_firmware.hex"
+    `define FIRMWARE "Software\\User_Codes\\bubble_sort_test\\bubble_sort_test_firmware.hex"
 `endif /*FIRMWARE*/
 
 `ifndef START_ADDRESS
